b6ccc3e7cbbe791d6b743093452be91ea0d6d0e6
Abbulu <abbulu.dulapalli@gmail.com>
2020-10-28 19:56:49 -0400

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Peddaabbulu Dulapalli on 10/28/20.
+//  Copyright © 2020 Dulapalli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
